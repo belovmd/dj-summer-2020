c11625718751fac4d35401bd1e0cad09c2add998
Maxim Belov <belovmd@gmail.com>
2020-07-18 12:12:15 +0300

    Add css styles

--- lesson/urls.py
+++ lesson/urls.py
@@ -4,4 +4,9 @@ app_name = 'lesson'
 
 urlpatterns = [
     path('', views.all_materials, name='all_materials'),
+    path(
+        '<int:year>/<int:month>/<int:day>/<slug:slug>',
+         views.material_details,
+         name='material_details',
+    ),
 ]
