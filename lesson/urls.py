e83ebf697d4ebd05ce5af30830a97d95b094cbcc
Maxim Belov <belovmd@gmail.com>
2020-07-25 12:22:46 +0300

    Add Django change password. EMAILS BROKEN

--- lesson/urls.py
+++ lesson/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from . import views
+from django.urls import reverse_lazy
 
 from django.contrib.auth import views as auth_views
 
 app_name = 'lesson'
+
+
+class MyHackedPassView(auth_views.PasswordResetView):
+    success_url = reverse_lazy('lesson:password_reset_done')
+
 
 urlpatterns = [
     path('', views.all_materials, name='all_materials'),
@@ -18,5 +24,10 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    path('password_reset/', MyHackedPassView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+            success_url=reverse_lazy('lesson:password_reset_complete'),
+    ), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
