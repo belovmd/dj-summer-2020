ba15b15c5b0f89646c151e6d3ba2229bcab647ea
Maxim Belov <belovmd@gmail.com>
2020-08-04 19:43:02 +0300

    Add user registration

--- lesson/forms.py
+++ lesson/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from . import models
-
+from django.contrib.auth.models import User
 
 class EmailMaterialForm(forms.Form):
     name = forms.CharField(max_length=255)
@@ -24,3 +24,21 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = models.Comment
         fields = ('name', 'email', 'body')
+
+
+class UserRegistrationForm(forms.ModelForm):
+
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Repeat password',
+                               widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'email')
+
+    def clean_password2(self):
+        cd = self.cleaned_data
+        if cd['password'] != cd ['password2']:
+            raise forms.ValidationError("passwords don't match")
+        return cd['password']
